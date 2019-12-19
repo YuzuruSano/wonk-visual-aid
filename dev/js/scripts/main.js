c5b0a700763e012b89192c641637e2bc73f9d93e
Yuzuru Sano(mac book) <sano@suitosha.co.jp>
2019-12-19 22:23:10 +0900

    update モジュール分割

--- dev/js/scripts/main.js
+++ dev/js/scripts/main.js
@@ -5,6 +5,7 @@ import MidiHandler from "modules/midiHandler";
 import LoadImg from "modules/LoadImg";
 import effLines from "modules/effLines";
 import effPulse from "modules/effPulse";
+import effImgCollage from "modules/effImgCollage";
 import 'p5/lib/addons/p5.sound';
 import * as p5 from 'p5';
 
@@ -202,113 +203,10 @@ if ($('#canvas2').length > 0) {
   const P5_2 = new p5(ss);
 }
 
-if ($('#canvas3').length > 0) {
-  let sss = (sk) => {
-    let layer1Images = [];
-    let layer2Images = [];
-    let layer3Images = [];
-
-    let layer1Items = [];
-    let layer2Items = [];
-    let layer3Items = [];
-    sk.preload = () => {
-      layer1Images.push(sk.loadImage('../images/collage/layer1_01.png'));
-      layer1Images.push(sk.loadImage('../images/collage/layer1_02.png'));
-      layer1Images.push(sk.loadImage('../images/collage/layer1_03.png'));
-      layer1Images.push(sk.loadImage('../images/collage/layer1_04.png'));
-      layer1Images.push(sk.loadImage('../images/collage/layer1_05.png'));
-
-      layer2Images.push(sk.loadImage('../images/collage/layer2_01.png'));
-      layer2Images.push(sk.loadImage('../images/collage/layer2_02.png'));
-      
-      layer3Images.push(sk.loadImage('../images/collage/layer3_01.png'));
-      layer3Images.push(sk.loadImage('../images/collage/layer3_02.png'));
-      layer3Images.push(sk.loadImage('../images/collage/layer3_03.png'));
-      layer3Images.push(sk.loadImage('../images/collage/layer3_04.png'));
-    }
-
-    sk.setup = async () => {
-      const canvas = sk.createCanvas(sk.windowWidth, sk.windowHeight);
-      canvas.parent('canvas3');
-      sk.imageMode(sk.CENTER);
-
-      sk.frameRate(30);
-
-      layer1Items = generateCollageItems(layer1Images, 100, sk.width / 2, sk.height / 2, sk.width, sk.height, 0.1, 0.5, 0, 0);
-      layer2Items = generateCollageItems(layer2Images, 150, sk.width / 2, sk.height / 2, sk.width, sk.height, 0.1, 0.3, -sk.HALF_PI, sk.HALF_PI);
-      layer3Items = generateCollageItems(layer3Images, 110, sk.width / 2, sk.height / 2, sk.width, sk.height, 0.1, 0.4, -sk.HALF_PI, sk.HALF_PI);
-
-      drawCollageItems(layer1Items);
-      drawCollageItems(layer2Items);
-      drawCollageItems(layer3Items);
-    }
-
-    sk.draw = () => {
-      const velocity = (mh.info.velocity) ? mh.info.velocity : 0.01;
-      sk.frameRate(30);
-      if (mh.info.note === 7) {
-        const mapVelocity = sk.map(velocity, 0, 127, 1, 4);
-        if (mapVelocity > 1 || 2 >= mapVelocity) layer1Items = generateCollageItems(layer1Images, sk.random(5, 15), sk.width / 2, sk.height / 2, sk.width, sk.height, 0.1, 0.5, 0, 0);
-        if (mapVelocity > 2 || 3 >= mapVelocity) layer2Items = generateCollageItems(layer2Images, sk.random(5, 8), sk.width / 6, sk.height / 6, sk.width, sk.height, 0.1, sk.random(0.3, 0.8), -sk.HALF_PI, sk.HALF_PI);
-        if ( 3 <= mapVelocity) layer3Items = generateCollageItems(layer3Images, sk.random(10, 60), sk.width / 2, sk.height / 2, sk.width / 2, sk.height / 2, 0.1, sk.random(0.2, 0.6), -sk.HALF_PI, sk.HALF_PI);
-
-        sk.clear();
-
-        drawCollageItems(layer1Items);
-        drawCollageItems(layer2Items);
-        drawCollageItems(layer3Items);
-      }
-    }
-
-    sk.keyReleased = (key) => {
-      if (key.key == 's' || key.key == 'S') saveCanvas(gd.timestamp(), 'png');
-      if (key.key == '1') layer1Items = generateCollageItems(layer1Images, sk.random(5, 15), sk.width / 2, sk.height / 2, sk.width, sk.height, 0.1, 0.5, 0, 0);
-      if (key.key == '2') layer2Items = generateCollageItems(layer2Images, sk.random(5, 8), sk.width / 2, sk.height / 2, sk.width, sk.height, 0.1, sk.random(0.3, 0.8), -sk.HALF_PI, sk.HALF_PI);
-      if (key.key == '3') layer3Items = generateCollageItems(layer3Images, sk.random(50, 100), sk.width / 2, sk.height / 2, sk.width, sk.height, 0.5, sk.random(0.6, 0.8), -sk.HALF_PI, sk.HALF_PI);
-
-      sk.clear();
-
-      drawCollageItems(layer1Items);
-      drawCollageItems(layer2Items);
-      drawCollageItems(layer3Items);
-    }
-
-    function generateCollageItems(layerImages, count, posX, posY, rangeX, rangeY, scaleStart, scaleEnd, rotationStart, rotationEnd) {
-      var layerItems = [];
-      for (var i = 0; i < count; i++) {
-        var index = i % layerImages.length;
-        var item = new CollageItem(layerImages[index]);
-        item.x = posX + sk.random(-rangeX / 2, rangeX / 2);
-        item.y = posY + sk.random(-rangeY / 2, rangeY / 2);
-        item.scaling = sk.random(scaleStart, scaleEnd);
-        item.rotation = sk.random(rotationStart, rotationEnd);
-        layerItems.push(item);
-      }
-      return layerItems;
-    }
-
-    function CollageItem(image) {
-      this.image = image;
-      this.x = 0;
-      this.y = 0;
-      this.rotation = 0;
-      this.scaling = 1;
-    }
-
-    function drawCollageItems(layerItems) {
-      for (var i = 0; i < layerItems.length; i++) {
-        sk.push();
-        sk.translate(layerItems[i].x, layerItems[i].y);
-        sk.rotate(layerItems[i].rotation);
-        sk.scale(layerItems[i].scaling);
-        sk.image(layerItems[i].image, 0, 0);
-        sk.pop();
-      }
-    }
-  }
-
-  const P5_3 = new p5(sss);
-}
+/**
+ * img collage
+ */
+effImgCollage(mh);
 
 
 if (module.hot) {
